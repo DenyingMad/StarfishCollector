5694b7f4a4f11c24e6bef28cdcf2bd87d10c8012
Danny <mezenov.201521@gmail.com>
2020-06-13 23:56:41 +0300

    first version

--- desktop/src/com/devilpanda/starfishcollector/desktop/DesktopLauncher.java
+++ desktop/src/com/devilpanda/starfishcollector/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.devilpanda.starfishcollector.StarfishCollectorAlpha;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new StarfishCollectorAlpha(), config);
 	}
 }
